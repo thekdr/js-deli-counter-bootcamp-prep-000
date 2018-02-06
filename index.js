3a047fed42026e7bca206e378575e3881009d4ac
Kristopher Ross <thekdr@yahoo.com>
2018-02-06 14:34:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ var katzDeliLine = [];
 
 function takeANumber (array, name) {
   array.push(name);
-  return (`Welome, ${name}. You are number ${array.length} in line.`)
+  return (`Welcome, ${name}. You are number ${array.length} in line.`)
 }
 
 function nowServing (array) {
   if (array.length === 0) {
-    return ("There is nobody waiting to be served.")
+    return ("There is nobody waiting to be served!")
   } else {
   var serving = array.shift();
-  return (`Currently serving ${serving}`);
+  return (`Currently serving ${serving}.`);
   }
 }
 
@@ -20,7 +20,7 @@ function currentLine(array) {
   } else { 
     var line = "The line is currently ";
     for (var i = 0; i < array.length; i++) {
-      line += `${array.length}. ${array[i]}, `
+      line += `${i + 1}. ${array[i]}, `
     }
     return (line)
   }  
